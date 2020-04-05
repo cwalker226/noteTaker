c5cb150f39d4f2e12cb819d0dc73d32200cd9c48
Walker, Corey <corey_walker@mcafee.com>
2020-04-04 22:49:06 -0700

    complete delete api call

--- server.js
+++ server.js
@@ -5,61 +5,74 @@ const uuid = require("uuid");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let notes = [];
+// let notes = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.delete('/api/notes/:id', (req, res) => {
+    console.log("api notes delete");
+    let noteID = req.params.id;
 
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-app.get("/api/notes", function(req, res) {
-    console.log("notes api get");
-    return fs.readFile(path.join(__dirname, "./db/db.json"), function(err, data) {
+    fs.readFile(path.join(__dirname, "./db/db.json"), function(err, data) {
         if (err) throw err;
-        notes = JSON.parse(data);
-        console.log(notes);
-        // returnArray.concat(notes);
-        // console.log(notes);
-        return res.json(notes);
+        let notes = JSON.parse(data);
+
+        const newNoteObj = notes.filter(function(val, ind, array) {
+            console.log(val);
+            return noteID != val.id;
+        });
+        console.log(newNoteObj);
+        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newNoteObj), function(err) {
+            if (err) throw err;
+            console.log("file wrote!");
+            res.json(newNoteObj);
+        });
     });
 });
 
 app.post("/api/notes", function(req, res) {
     console.log(req.body);
+    const newNote = [{id: uuid.v4(), title: req.body.title, text: req.body.text}];
 
-    notes.push({id: uuid.v4(), title: req.body.title, text: req.body.text});
-   
-    console.log("notes api post");
-    console.log(JSON.stringify(notes));
-    return fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(notes), function(err) {
+    let notes;
+
+    fs.readFile(path.join(__dirname, "./db/db.json"), function(err, data) {
         if (err) throw err;
-        console.log("file wrote!");
+        console.log(data);
+        notes = JSON.parse(data);
+        console.log(notes);
+
+        const newNotesObj = notes.concat(newNote);
+   
+        console.log("notes api post");
+        console.log(JSON.stringify(newNotesObj));
+        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newNotesObj), function(err) {
+            if (err) throw err;
+            console.log("file wrote!");
+            res.json(newNotesObj);
+        });
+    });
+
+    
+});
+
+app.get("/api/notes", function(req, res) {
+    console.log("notes api get");
+    fs.readFile(path.join(__dirname, "./db/db.json"), function(err, data) {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        res.json(notes);
     });
 });
 
-app.delete('api/notes/:id', (req, res) => {
-    console.log("api notes delete");
-    let noteID = req.params.id;
-    
-    console.log(noteID);
-    for(var i = 0; i < notes.length; i++){
-        console.log(notes[i].id);
-        if(noteID === notes[i].id){
-            delete notes[i];
-        }
-    }
-    return fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(notes), function(err) {
-        if (err) throw err;
-        console.log("file wrote!");
-        
-    });
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
